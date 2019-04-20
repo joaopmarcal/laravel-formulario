47aa37ec4bad0dff282625ca29115c7fc1170a2d
João Paulo <paulospc21@gmail.com>
2019-04-20 10:21:14 -0300

    iniciando com validações de dados

--- app/Http/Controllers/Admin/ClientsController.php
+++ app/Http/Controllers/Admin/ClientsController.php
@@ -37,6 +37,17 @@ class ClientsController extends Controller
      */
     public function store(Request $request)
     {
+        $maritalStatus = implode(',',array_keys(Client::MARITAL_STATUS));
+        $this->validate($request, [
+            'name'                  => 'required|max:255',
+            'document_number'       => 'required',
+            'email'                 => 'required|email',
+            'phone'                 => 'required',
+            'date_birth'            => 'required|date',
+            'marital_status'        => "required|in:$maritalStatus",
+            'sex'                   => 'required|in:m,f',
+            'physical_disability'   => 'max:255'
+        ]);
         $data = $request->all();
         $data['defaulter'] = $request->has('defaulter');
         Client::create($data);
