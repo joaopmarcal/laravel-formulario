ea30c86cdb53f2f393dd739d93aa3f32e612ff89
João Paulo <paulospc21@gmail.com>
2019-04-20 15:59:27 -0300

    exclusão de dados em modo resource

--- resources/views/admin/clients/show.blade.php
+++ resources/views/admin/clients/show.blade.php
@@ -3,6 +3,12 @@
 @section('content')
     <h3>Ver Cliente</h3>
     <a class="btn btn-primary" href="{{ route('clients.edit',['client' => $client->id]) }}">Editar</a>
+    <a class="btn btn-danger" href="{{ route('clients.destroy',['client' => $client->id]) }}"
+        onclick="event.preventDefault();if(confirm('Deseja excluir este item?')){document.getElementById('form-delete').submit();}">Excluir</a>
+    <form id="form-delete" style="display: none" action="{{ route('clients.destroy',['client' => $client->id]) }}" method="post">
+        {{csrf_field()}}
+        {{method_field('DELETE')}}
+    </form>
     <a class="btn btn-primary" href="{{ route('clients.store',['client' => $client->id]) }}">Voltar</a>
     <br><br>
     <table class="table table-bordered">
