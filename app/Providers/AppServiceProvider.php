bed541cbc02d38ac8bab9e7f1a4b35420ead8b67
João Paulo <paulospc21@gmail.com>
2019-04-25 08:57:59 -0300

    fim do módulo validações e formulários

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Code\Validator\Cnpj;
+use Code\Validator\Cpf;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +18,10 @@ class AppServiceProvider extends ServiceProvider
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
         $platform = \Schema::getConnection()->getDoctrineSchemaManager()->getDatabasePlatform();
         $platform->registerDoctrineTypeMapping('enum','string');
+        \Validator::extend('document_number', function($attribute,$value,$parameters,$validator){
+           $documentValidator = $parameters[0] == 'cpf' ? new Cpf() : new Cnpj();
+           return $documentValidator->isValid($value);
+        });
     }
 
     /**
